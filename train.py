feb8fd7428c56c8c31aa1786422e4e5e6d08bb2c
Suraj Sharma <144049668+Suraj-Sharma001@users.noreply.github.com>
2025-05-23 14:53:31 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO('models/yolov8n.pt')
+model = YOLO('models/yolov8n.pt')  // yolo v8 neno
 
 def main():
+    // training the data 
     model.train(data='Dataset/SplitData/data.yml', epochs=3)
 
 if __name__ == '__main__':
